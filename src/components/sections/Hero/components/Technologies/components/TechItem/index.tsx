682216cece398ee68b880342afc611370a9d9187
kbujak09 <kaccpper2@gmail.com>
2025-10-08 06:10:51 +0200

    Static techs

--- src/components/sections/Hero/components/Technologies/components/TechItem/index.tsx
+++ src/components/sections/Hero/components/Technologies/components/TechItem/index.tsx
@@ -1,7 +1,18 @@
 import styles from './index.module.scss';
 
-export default function TechItem() {
+interface TechItemProps {
+  image: string;
+  name: string;
+  description: string;
+}
+
+export default function TechItem({image, name, description}: TechItemProps) {
   return (
-    <div className={styles.container}>item</div>
+    <div className={styles.container}>
+      <img src={image} alt={name} className={styles.image} />
+      <div className={styles.name}>
+        {name}
+      </div>
+    </div>
   )
 }
--- src/components/sections/Hero/components/Technologies/index.tsx
+++ src/components/sections/Hero/components/Technologies/index.tsx
@@ -1,4 +1,21 @@
+import TechItem from './components/TechItem';
+
+import ReactLogo from '@/assets/images/techs/react.svg';
+import ExpressLogo from '@/assets/images/techs/express.svg';
+import MongoLogo from '@/assets/images/techs/mongo.svg';
+import SassLogo from '@/assets/images/techs/sass.svg';
+import TsLogo from '@/assets/images/techs/typescript.svg';
+
 import styles from './index.module.scss';
+
+const techs = [
+  { name: 'React', image: ReactLogo, description: 'A library for building UIs' },
+  { name: 'Express', image: ExpressLogo, description: 'Node.js framework' },
+  { name: 'MongoDB', image: MongoLogo, description: 'NoSQL database' },
+  { name: 'Sass', image: SassLogo, description: 'CSS preprocessor' },
+  { name: 'TypeScript', image: TsLogo, description: 'Typed superset of JavaScript' },
+];
+
 
 export default function Technologies() {
   return (
@@ -6,7 +23,13 @@ export default function Technologies() {
       <div className={styles.title}>
         Tools and Technologies
       </div>
-      <div className={styles.techs}></div>
+      <div className={styles.techs}>
+        {
+          techs.map((item) => (
+            <TechItem name={item.name} image={item.image}/>
+          ))
+        }
+      </div>
     </div>
   )
 }
